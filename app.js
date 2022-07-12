6490ef32deff7c42ee92cc85084f5fdb039e7f01
Your Name <you@example.com>
2022-07-12 17:43:30 -0600

    getting there

---
+++ app.js
@@ -0,0 +1,18 @@
+let userNames = [`josh`, `james`, `marc`, `lucas`]
+
+
+let counter = 0
+
+while(counter < userNames.length){
+      lowerUserName = userNames.toLowerCase()
+      doesInclude = userNames.includes(`winner`)
+    if(doesInclude === true){
+        console.log(`We have a winner (doesInclude)`)
+    }
+    counter++
+
+    
+}
+let lowerUserName = userNames.toLowerCase()
+let doesInclude = userNames.includes(`winner`)
+ 
