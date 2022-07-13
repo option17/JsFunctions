8ee3b0c0515adf81bbfb87ce3d03cea4d3b07963
Your Name <you@example.com>
2022-07-12 18:15:18 -0600

    Changed coding in line 9, took out the excess.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let counter = 0
 while(counter < userNames.length){
     
       let lowerUserName = userNames[counter].toLowerCase()
-      console.log(lowerUserName = userNames[counter].toLowerCase())
+      console.log(lowerUserName )
       let doesInclude = userNames[counter].includes(`winner`)
     if(doesInclude === true){
         console.log(`We have a winner: ${userNames[counter]}`);
